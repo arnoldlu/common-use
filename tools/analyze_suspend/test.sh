e58e01774c52b2afd211405b423ac21a6538fcea
Arnold Lu <arnoldlu@qq.com>
2017-04-14 10:38:35 +0800

    add setenforece for kernel v4.9
    Signed-off-by: Arnold Lu <arnoldlu@qq.com>

--- tools/analyze_suspend/test.sh
+++ tools/analyze_suspend/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#adb root
+#adb shell setenforce 0
 ./analyze_suspend.py -verbose -adb  /usr/bin/adb -rtcwake 10 -multi 5 20 -f -m mem
 ./analyze_suspend.py -verbose -adb  /usr/bin/adb -rtcwake 10 -multi 5 20 -f -m freeze
